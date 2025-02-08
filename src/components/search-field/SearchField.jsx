8e57161b994129330b5fa5ef57a25a29a22e802d
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2025-02-08 19:21:37 +0300

    Implements ability to get and reset search value

--- src/components/search-field/SearchField.jsx
+++ src/components/search-field/SearchField.jsx
@@ -2,15 +2,22 @@ import styles from "./search-field.module.css";
 import resetImage from "./reset.svg";
 import searchImage from "./search.svg";
 
-export const SearchField = () => {
+export const SearchField = ({ value, onChange }) => {
   return (
     <div className={styles.root}>
       <img src={searchImage} className={styles.search} />
       <input
         className={styles.input}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
         placeholder="Телефоны, яблоки, груши..."
       />
-      <img src={resetImage} className={styles.reset} />
+      <img
+        src={resetImage}
+        className={styles.reset}
+        style={!value ? { display: "none" } : null}
+        onClick={() => onChange("")}
+      />
     </div>
   );
 };
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { Button } from "../button/Button";
 import { SearchField } from "../search-field/SearchField";
 
 import styles from "./search.module.css";
 
 export const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className={styles.root}>
-      <form className={styles.search}>
-        <SearchField />
+      <form
+        className={styles.search}
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <SearchField value={searchValue} onChange={setSearchValue} />
         <Button title="Искать" />
       </form>
     </div>
